897c52999acbe6c341c172c8e94d5249b295b0c7
Gorak8050 <gorak8050@gmail.com>
2021-02-23 23:12:41 +0530

    Update 3.2.1- New REPO Pushed
    Update 3.2.1
    
    New REPO Pushed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
-import Home from './pages/Home'
-import Report from './pages/Report'
-import Products from './pages/Products'
-import ProductDetail from './pages/ProductDetail'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
-class App extends Component {
-  render() {
-    return (
-      <>
-      <Router>
-        <Navbar />
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/reports' component={Report} />
-            <Route path='/products' component={Products} />
-            <Route path='/productdetails' component={ProductDetail} />
-          </Switch>
-      </Router>
-
-      </>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React App Testing
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        Coming Soon..
+      </header>
+    </div>
+  );
 }
 
 export default App;
